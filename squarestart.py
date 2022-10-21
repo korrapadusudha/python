3cadc60e8b1df80a22ed01b07537af8cfa7bb7ea
korrapadusudha <108741569+korrapadusudha@users.noreply.github.com>
2022-10-21 12:39:42 +0530

    Update squarestart.py

--- squarestart.py
+++ squarestart.py
@@ -1,19 +1,13 @@
 n=int(input("enter any number:"))
 for i in range(n):
-    print(" "*(n-1-i)+("* ")*(i+1))
-for i in range(n):
-    print(" "*(i+1)+("* ")*(n-1-i))
-
-"""n=int(input("enter any number:"))
-for i in range(n):
     for j in range(n):
         if (i==0 or i==n-1 or j==0 or j==n-1):
             print("*",end=" ")
         else:
             print(" ",end=" ")
-    print()"""
-    
-
+    print()
+      
+                (OR)
 """side = int(input("Please Enter any Side of a Square  : "))
 for i in range(side):
     for j in range(side):
